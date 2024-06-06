51501c8d97bde5173ae3d5acbbdff81cd603480d
s.shimizu <s-shimizu@tsumikiinc.com>
2024-06-06 20:55:25 +0900

    シャッフル関数実装

--- src/app/tools/shuffle/page.tsx
+++ src/app/tools/shuffle/page.tsx
@@ -1,3 +1,26 @@
 export default function Page() {
-  return <div>シャッフル</div>;
+  let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const shuffle = (arr: Array<number>) => {
+    let ans = arr.slice();
+    for(let i = ans.length - 1; i > 0; i--){
+      let r = Math.floor(Math.random() * (i + 1));
+      let tmp = ans[i];
+      ans[i] = ans[r];
+      ans[r] = tmp;
+    }
+    return ans;
+  }
+  return (
+    <main>
+      <h1>シャッフル</h1>
+      <div>
+        <ul>
+          {shuffle(data).map((v, i) => (
+            <li key={i}>{v}</li>
+          ))}
+        </ul>
+      </div>
+      <p>Fisher–Yatesアルゴリズム</p>
+    </main>
+  );
 }
