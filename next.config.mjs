494417e98ed1c4519642ccc61f19721b22cc3075
s.shimizu <s-shimizu@tsumikiinc.com>
2024-06-06 20:17:46 +0900

    sass導入

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+import path from 'path';
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   output: "export",
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 };
 
 export default nextConfig;
