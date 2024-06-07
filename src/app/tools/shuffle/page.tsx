7e5bf1d1aa6758e356df20011545b153fe6ba9ae
s.shimizu <s-shimizu@tsumikiinc.com>
2024-06-07 13:50:31 +0900

    入力データをシャッフルする（仮実装）

--- src/app/tools/shuffle/page.tsx
+++ src/app/tools/shuffle/page.tsx
@@ -1,24 +1,71 @@
 'use client';
 import React, { useState } from 'react';
+import { useForm, useFieldArray } from 'react-hook-form';
 import Button from '../../../components/elements/Button';
 
 export default function Page() {
-  let base = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let base: Array<String> = [];
   const [data, setData] = useState(base.slice());
-  const shuffle = (arr: Array<number>) => {
-    let data = arr.slice();
-    for(let i = data.length - 1; i > 0; i--){
-      let r = Math.floor(Math.random() * (i + 1));
-      let tmp = data[i];
-      data[i] = data[r];
-      data[r] = tmp;
+
+  const { register, control, handleSubmit, getValues } = useForm({
+    defaultValues: {
+      items: [{ value: '' }]
     }
-    return data;
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'items'
+  });
+
+  const shuffle = (arr: Array<number>) => {
+    let ans = arr.slice();
+    for(let i = ans.length - 1; i > 0; i--){
+      let r = Math.floor(Math.random() * (i + 1));
+      let tmp = ans[i];
+      ans[i] = ans[r];
+      ans[r] = tmp;
+    }
+    return ans;
   }
+
+  const onSubmit = (data) => {
+    const items = getValues('items');
+    const values = items.map(item => item.value);
+    base = values.slice();
+
+    const lastItem = items[items.length - 1];
+    if (lastItem.value !== '') {
+      append({ value: '' });
+    }
+  };
+
+  const onShuffle = () => {
+    const items = getValues('items');
+    const values = items.map(item => item.value);
+    base = values.slice();
+  
+    // シャッフル関数を呼び出す
+    const shuffledValues = shuffle(base);
+  
+    // シャッフルされたデータをセットする
+    setData(shuffledValues);
+  };
 
   return (
     <main>
       <h1>シャッフル</h1>
+      <h2>取り込みデータ</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {fields.map((item, index) => (
+          <div key={item.id}>
+            <input {...register(`items.${index}.value`)} />
+            <button type="button" onClick={() => remove(index)}>x</button>
+          </div>
+        ))}
+        <input type="submit" style={{ display: 'none' }} />
+      </form>
+      <Button onClick={onShuffle}>シャッフル</Button>
+      <h2>シャッフル結果</h2>
       <div>
         <ul>
           {data.map((v, i) => (
@@ -26,7 +73,6 @@ export default function Page() {
           ))}
         </ul>
       </div>
-      <Button onClick={() => setData(shuffle(base))} text="シャッフル" />
       <p>Fisher–Yatesアルゴリズム</p>
     </main>
   );
--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 interface ButtonProps {
   onClick: () => void;
-  text: string;
+  children: React.ReactNode; // children プロパティを追加
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  );
+const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+  return <button onClick={onClick}>{children}</button>;
 };
 
 export default Button;
