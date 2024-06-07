cb067cc6d9caaeb5c58959c4274136354b4334a4
s.shimizu <s-shimizu@tsumikiinc.com>
2024-06-07 13:13:06 +0900

    シャッフルを任意で行えるように変更

--- src/app/tools/shuffle/page.tsx
+++ src/app/tools/shuffle/page.tsx
@@ -1,25 +1,32 @@
+'use client';
+import React, { useState } from 'react';
+import Button from '../../../components/elements/Button';
+
 export default function Page() {
-  let data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let base = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [data, setData] = useState(base.slice());
   const shuffle = (arr: Array<number>) => {
-    let ans = arr.slice();
-    for(let i = ans.length - 1; i > 0; i--){
+    let data = arr.slice();
+    for(let i = data.length - 1; i > 0; i--){
       let r = Math.floor(Math.random() * (i + 1));
-      let tmp = ans[i];
-      ans[i] = ans[r];
-      ans[r] = tmp;
+      let tmp = data[i];
+      data[i] = data[r];
+      data[r] = tmp;
     }
-    return ans;
+    return data;
   }
+
   return (
     <main>
       <h1>シャッフル</h1>
       <div>
         <ul>
-          {shuffle(data).map((v, i) => (
+          {data.map((v, i) => (
             <li key={i}>{v}</li>
           ))}
         </ul>
       </div>
+      <Button onClick={() => setData(shuffle(base))} text="シャッフル" />
       <p>Fisher–Yatesアルゴリズム</p>
     </main>
   );
